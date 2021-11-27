adc13ca277138b0d58c2cd30f235705ea66794ef
gretarmark <gretarmark@gmail.com>
2021-11-27 13:16:15 +0100

    MOC2

--- Exercise_2/FinalAssignment_GaussianForeground.cpp
+++ Exercise_2/FinalAssignment_GaussianForeground.cpp
@@ -41,10 +41,13 @@ int main(int argc, char** argv) {
 
 
 
+    
+
     // Create a VideoCapture object and open the input file
     // If the input is the web camera, pass 0 instead of the video file name
-    VideoCapture capture("TwoObjects.mp4");
+    //VideoCapture capture("TwoObjects.mp4");
     //VideoCapture cap2("TwoObjects.mp4");
+    VideoCapture capture(0);
 
 
     // Open the video file
@@ -60,12 +63,23 @@ int main(int argc, char** argv) {
     // The Mixture of Gaussian object
     // used with all default parameters
     
+
+
+        /*   createBackgroundSubtractorMOG2(history, varThreshold, detectShadow);   */
+                             // history tells us how many frames should be used to model the background
+                             // varThreshold correlates the value of the weight of the pixels on the current frame to model
+                             //     - low values on varThreshold tend to create false objects.
     //BackgroundSubtractorMOG2 mog; // also BackgroundSubtractorMOG2 that removes shadows
     //Ptr<BackgroundSubtractor> mog = createBackgroundSubtractorMOG2();
     //Ptr<BackgroundSubtractor> mog = createBackgroundSubtractorMOG2(20, 16, true);
-    Ptr<BackgroundSubtractor> mog = createBackgroundSubtractorKNN();
-    system("Pause");
+    Ptr<BackgroundSubtractor> mog = createBackgroundSubtractorMOG2(1, 150, false);
+    //Ptr<BackgroundSubtractor> mog = createBackgroundSubtractorKNN();
+    //system("Pause");
+
+    vector<vector<Point> > contours;
     
+    
+
     int counter = 0;
 
     bool stop(false);
@@ -98,7 +112,15 @@ int main(int argc, char** argv) {
         // Complement the image
 
         //threshold(foreground, foreground, 128, 255, cv::THRESH_BINARY_INV);
-        threshold(foreground, foreground, 190, 255, cv::THRESH_BINARY_INV);
+        //threshold(foreground, foreground, 190, 255, cv::THRESH_TRUNC);
+        
+        //Mat mask 
+        // Color thresholding
+        //inRange(frame, Scalar(100, 125, 100), Scalar(255, 200, 255), foreground);
+        
+        // Finding contours in the image
+        //findContours(foreground, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        /*cout << "contours: " << contours[0][0].x << endl;*/
 
         // show foreground
         cv::imshow("Extracted Foreground", foreground);
